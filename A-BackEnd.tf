1f269c905f62779dbd4ce390a532ce8386db6288
Larvarious McDonald <larvariousmcdonald@outlook.com>
2026-05-27 11:04:10 -0500

    Create A-BackEnd.tf

---
+++ A-BackEnd.tf
@@ -0,0 +1,10 @@
+terraform {
+    backend "s3" {
+        bucket = "armageddonconq"     #unique name of the s3 buckect  
+        key = "armageddon"            #name of the file 
+        region = "ap-northeast-1"      
+}
+}
+
+
+
